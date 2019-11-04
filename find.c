e38fd67e46c73d43782fd45a93d0718d6a0d995d
Yuri Paoloni <yuri.paoloni3@gmail.com>
2019-11-04 19:13:06 +0100

    implementato lettura occorenze file

--- find.c
+++ find.c
@@ -1,15 +1,28 @@
 #include "find.h"
 
-// Program to implement KMP Algorithm in C
-int main(void)
-{
-	char* text = "ABaaaaaaaaaaaaaaaAAAAAAACABAABCABAC";
-	char* pattern = "CAB";
+int main(int argc,char* argv[]){
 
-	int n = strlen(text);
-	int m = strlen(pattern);
+	char currentLine[255]; //conterrà la riga corrente
+	char line[255];
+	char word[30];
+	int count = 0;
 
-	KMP(text, pattern, n, m);
+	FILE * fInput = fopen(argv[2], "r"); //apre il file passato come parametro in lettura
+	if(fInput == NULL){ //check sull'apertura del file
+        fprintf(stderr, "Cannot open %s, exiting. . .\n", argv[2]);
+        exit(1);
+    }
+
+	printf("Enter the word to search: ");
+	scanf("%s", word);
+
+	while(fgets(currentLine, sizeof(currentLine), fInput) != NULL){ //legge tutte le righe del file
+		int n = strlen(currentLine);
+		int m = strlen(word);
+		KMP(currentLine, word, n, m);
+	}
+
+	fclose(fInput);
 
 	return 0;
 }
