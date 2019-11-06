d41abac78dc96553e7388c1f371fb718d897a970
Yuri Paoloni <yuri.paoloni3@gmail.com>
2019-11-06 17:55:17 +0100

    definito nuove funzioni

--- kmp.h
+++ kmp.h
@@ -2,4 +2,8 @@
 #include<stdlib.h>
 #include<string.h>
 
-void KMP(const char* X, const char* Y, int m, int n, int line);
+int KMP(const char* X, const char* Y, int m, int n, int line, int index);
+
+void printPosition(int index);
+
+int getTotalOccurences(int index);
