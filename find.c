13dd5e7db2ae34de4704bef0bfed25d746ab9fa4
Yuri Paoloni <yuri.paoloni3@gmail.com>
2019-11-06 18:03:05 +0100

    aggiunto strutture per salvataggio dati e stampa dati formattati correttamente

--- find.c
+++ find.c
@@ -3,20 +3,43 @@
 struct filePath{
 	char path[255];
 	int fileOccurences;
+	int position[255][255];
+};
+
+struct fileWord{
+	char word[30];
+	int totalOccurences;
+	struct filePath fPath[2];
 };
 
 int main(int argc,char* argv[]){
 
 	struct filePath fPath[2];
+	struct fileWord fWord[1];
 	char currentLine[255]; //conterrà la riga corrente
 	char path[255];
 	char word[30];
     int i, ch;
 	int k = 0;
 
-	FILE * fInput = fopen(argv[1], "r"); //apre il file passato come parametro in lettura
+	if(argc == 1){
+		printf("Il programma find consente di individuare il numero di occorrenze di un insieme di stringhe all'interno di un gruppo di file.\r\n");
+		printf("Per eseguire il programma utilizzare i seguenti parametri:\r\n");
+		printf(" find --word|-w <inputfile> : per stampare il report alla fine dell'esecuzione\r\n");
+		printf("Parametri opzionali:\r\n");
+		printf(" --output|-o <outputfile> : per salvare il report su un file particolare\r\n");
+		printf(" --exclude|-e <ext> : durante la fase di analisi sara' possibile ignorare i file con specifiche estensioni\r\n");
+		printf(" --verbose|-v : per visionare il processo di analisi\r\n");
+		printf("Una volta generato il file di report, il programma find potra' essere usato per recuperare\r\n");
+		printf("le informazioni salvate con i seguenti comandi:\r\n");
+		printf(" find --report|-r <reportfile> --show <word> <n> : stampare la lista del file dove occorre almeno <n> volte la parola <word>\r\n");
+		printf(" find --report|-r <reportfile> --show <word> --file <file> : stampare tutte le posizioni dove la parola <word> occorre nel <file>\r\n");
+		return 0;
+	}
+	
+	FILE * fInput = fopen(argv[2], "r"); //apre il file passato come parametro in lettura
 	if(fInput == NULL){ //check sull'apertura del file
-    	fprintf(stderr, "Cannot open %s, exiting. . .\n", argv[1]);
+    	fprintf(stderr, "Cannot open %s, exiting. . .\n", argv[2]);
     	exit(1);
     }
 
@@ -31,10 +54,9 @@ int main(int argc,char* argv[]){
 	fclose(fInput);
 
 	printf("Enter the word to search: ");
-	scanf("%s", word);
+	scanf("%s", fWord[0].word);
 
-	printf("WORD <%s> \r\n", word);
-	printf("TOTAL <%d> \r\n", 0);
+	fWord[0].totalOccurences = 0;
 
 	for(int j = 0; j<2; j++){
 		FILE * fp = fopen(fPath[j].path, "r"); //apre il file passato come parametro in lettura
@@ -43,7 +65,8 @@ int main(int argc,char* argv[]){
 			exit(1);
 		}
 
-		int count = 0;
+		int countLine = 0;
+		fPath[j].fileOccurences = 0;
 
 		while(!feof(fInput)){
 			for (i = 0; (i < (sizeof(currentLine)-1) && ((ch = fgetc(fp)) != EOF) && (ch != '\n')); i++){
@@ -51,17 +74,21 @@ int main(int argc,char* argv[]){
 			}
 			currentLine[i] = '\0';
 			int n = strlen(currentLine);
-			int m = strlen(word);
-			KMP(currentLine, word, n, m, count);
-			count = count + 1;
+			int m = strlen(fWord[0].word);
+			fPath[j].fileOccurences = fPath[j].fileOccurences + KMP(currentLine, fWord[0].word, n, m, countLine, j);
+			countLine = countLine + 1;
 		}
-
-		printf("FILE <%s> \r\n", /*path del file*/ fPath[j].path);
-		printf("OCCURENCES <%d>	\r\n", 0);
-		printf("<linea> <posizione>\r\n");
+		fWord[0].totalOccurences = fWord[0].totalOccurences + getTotalOccurences(j);
 		fclose(fp);
 	}
 
+	printf("WORD %s \r\n", fWord[0].word);
+	printf("TOTAL %d \r\n", fWord[0].totalOccurences);
+	for(int h = 0; h<2; h++){
+		printf("FILE %s \r\n", /*path del file*/ fPath[h].path);
+		printf("OCCURENCES %d	\r\n", fPath[h].fileOccurences);
+		printPosition(h);
+	}
 	printf("\r\n"); //il file termina con una riga vuota
 
 	return 0;
