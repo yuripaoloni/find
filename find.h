435cf44c7f600d35e0f48465cb146a3cc6ea61fd
Yuri Paoloni <yuri.paoloni3@gmail.com>
2019-11-12 23:24:26 +0100

    aggiunto definizione funzione

--- find.h
+++ find.h
@@ -1,5 +1,11 @@
 #include "kmp.h"
 
+void writeFile(fWord *w, fWord *wordHead, char * outputFile);
+
+void execute(char *inputFile);
+
+void print(fWord *w, fWord *wordHead);
+
 char * getLineOfAnySize(FILE* fp, size_t typicalSize, int *endOfLineDetected,size_t *nrOfCharRead);
 
 
