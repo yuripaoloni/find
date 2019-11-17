b8f87fa03a8e763673fb02458d5202ded1d5505a
Yuri Paoloni <yuri.paoloni3@gmail.com>
2019-11-17 21:19:48 +0100

    aggiunto definizione nuove funzioni

--- find.h
+++ find.h
@@ -1,9 +1,14 @@
 #include<time.h>
+#include<dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include "kmp.h"
+
 
 char * getLineOfAnySize(FILE* fp, size_t typicalSize, int *endOfLineDetected,size_t *nrOfCharRead);
 
-void execute(char *inputFile, char *excluded, int num);
+void execute(char *wordFile, char *inputFile, char *excluded, int num);
 
 void writeFile(fWord *w, fWord *wordHead, char * outputFile);
 
@@ -16,6 +21,10 @@ void getWordOccurences(char *word, char *file, char *fileToCheck);
 void freeMemory();
 
 char *get_filename_ext(char *filename);
+
+int is_regular_file(const char *path);
+
+void listFilesRecursively(char *path, char *recursive, int a, char *exclude);
 
 
 
