7bc4c58b3f3f1c57a129a79e031dc27a5bb6e968
Yuri Paoloni <yuri.paoloni3@gmail.com>
2019-11-17 21:19:32 +0100

    aggiunta struttura per linee file di input

--- kmp.h
+++ kmp.h
@@ -4,18 +4,25 @@
 
 struct fileList{
     char *path;
+    char *directory;
     struct fileList *next;
 };
 
 struct filePath{
     char *path;
+    char *directory;
     int fileOccurrences;
     struct OccurrencesPosition *position;
     struct filePath *next;
 };
 
+struct lineList{ //structure with all the line of the input file
+    char *line;
+    struct lineList *next;
+};
+
 struct fileWord{
-	char word[50];
+	char *word;
 	int totalOccurences;
     struct filePath *p;
     struct fileWord *next;
@@ -31,6 +38,8 @@ typedef struct filePath fPath;
 typedef struct fileWord fWord;
 typedef struct OccurrencesPosition fPosition;
 typedef struct fileList fList;
+
+typedef struct lineList llist;
 
 fPosition * getHead();
 
