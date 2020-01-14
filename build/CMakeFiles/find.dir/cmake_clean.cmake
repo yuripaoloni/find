914a5be01a77bd75875305d0f9bde950825567a3
Yuri Paoloni <yuri.paoloni3@gmail.com>
2020-01-14 23:01:51 +0100

    created CMake

---
+++ build/CMakeFiles/find.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/find.dir/find.c.o"
+  "CMakeFiles/find.dir/kmp.c.o"
+  "CMakeFiles/find.dir/struct.c.o"
+  "CMakeFiles/find.dir/test.c.o"
+  "find.pdb"
+  "find"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/find.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/find.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for find.
+# This may be replaced when dependencies are built.
